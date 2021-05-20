0df95deda78fb4b4f4d92d91b37344f1958aff37
Emir Özmen <trolllca@hotmail.com>
2021-05-20 21:59:15 +0300

    edit

--- emir/nfa2dfa.py
+++ emir/nfa2dfa.py
@@ -8,17 +8,17 @@ symbols = lines[0].rstrip().split()
 nfa_states = lines[1].rstrip().split()
 nfa_startingState = lines[2].rstrip()
 nfa_finalStates = lines[3].rstrip().split()
-nfa_delta = []
+nfa_transitions = []
 for line in lines[4:]:
     part = line.rstrip().split()
-    nfa_delta.append(part)
+    nfa_transitions.append(part)
 
 f.close()
 
 #For getting the next state.
 def get_nextState(current_state, movement):
     potential_states = []
-    for state in nfa_delta:
+    for state in nfa_transitions:
         if state[0] == current_state and state[1] == movement:
             potential_states.append(state[2])
     if len(potential_states) == 0:
@@ -28,20 +28,19 @@ def get_nextState(current_state, movement):
 
 ########### Epsilon-NFA to NFA ###########
 
-#For getting the next state with ε expression ĞĞĞ
-def epsilon_closure(state):
+#For getting the states, which have connections with epsilon.
+def related_epsilon(state):
     related_states = [state]
-    history = [state]
+    registor = [state]
 
-    while len(history) != 0:
-        next_states = get_nextState(history[0], 'Îµ')
-
+    while len(registor) != 0:
+        next_states = get_nextState(registor[0], 'Îµ')
         if next_states is not None:
             for state in next_states:
                 if state not in related_states:
                     related_states.append(state)
-                    history.append(state)
-        history.pop(0)
+                    registor.append(state)
+        registor.pop(0)
 
     return related_states
 
@@ -50,7 +49,7 @@ def epsilon_closure(state):
 def nextState_without_epsilon(current_state,movement):
     potential_states = []
 
-    lambda_cl = epsilon_closure(current_state)
+    lambda_cl = related_epsilon(current_state)
     temporary_states = []
 
     for state in lambda_cl:
@@ -61,7 +60,7 @@ def nextState_without_epsilon(current_state,movement):
                     temporary_states.append(s)
 
     for state in temporary_states:
-        temporary_states = epsilon_closure(state)
+        temporary_states = related_epsilon(state)
         if temporary_states is not None:
             for s in temporary_states:
                 if s not in potential_states:
@@ -75,12 +74,12 @@ def nextState_without_epsilon(current_state,movement):
 ########### Epsilon-NFA to NFA ###########
 
 
-#Assigning the old values.
-dfa_startingState = epsilon_closure(nfa_startingState)
+#Assigning the DFA variables from translation.
+dfa_startingState = related_epsilon(nfa_startingState)
 dfa_startingState.sort()
 
 dfa_states = [dfa_startingState]
-dfa_delta = []
+dfa_transitions = []
 dfa_final_states = []
 
 #For fixing the final states.
@@ -88,16 +87,16 @@ for f in nfa_finalStates:
     if f in dfa_startingState:
         dfa_final_states.append(dfa_startingState)
 
-re_states = [dfa_startingState]
-history = [dfa_startingState]
+remaining_states = [dfa_startingState]
+registor = [dfa_startingState]
 
-#Main ĞĞĞ
-while len(re_states) != 0 :
+
+while len(remaining_states) != 0 :
     #For checking all the states
     for letter in symbols:
         states = []
-        for re_state in re_states[0]:
-            next_states = nextState_without_epsilon(re_state,letter)
+        for remaining_state in remaining_states[0]:
+            next_states = nextState_without_epsilon(remaining_state,letter)
             if next_states is not None:
                 for next_state in next_states:
                     if next_state not in states:
@@ -105,12 +104,12 @@ while len(re_states) != 0 :
 
 
         if len(states) != 0:
-            dfa_delta.append([re_states[0], letter, states])
+            dfa_transitions.append([remaining_states[0], letter, states])
             states.sort()
 
             if states not in dfa_states:
                 dfa_states.append(states)
-                re_states.append(states)
+                remaining_states.append(states)
 
 
             for f_state in nfa_finalStates:
@@ -118,8 +117,8 @@ while len(re_states) != 0 :
                     dfa_final_states.append(states)
         else:
             #trap state.
-            dfa_delta.append([re_states[0], letter, 'trap'])
-    re_states.pop(0)
+            dfa_transitions.append([remaining_states[0], letter, 'trap'])
+    remaining_states.pop(0)
 
 
 ########### Sending to .txt file. ###########
@@ -142,10 +141,8 @@ for fs in dfa_final_states:
     f.write(f"{''.join(list(fs))} ")
 f.write('\n')
 
-for state in dfa_delta:
+for state in dfa_transitions:
     f.write(f"{''.join(list(state[0]))} {state[1]} {''.join(list(state[2]))}")
     f.write('\n')
 
-f.write('\b')
-
-f.close()
+print("done.")
